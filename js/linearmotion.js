473862658595a2038870efc1618d5abeda5b508b
Mikey Rooney <m.rooney93@gmail.com>
2015-03-23 19:55:01 +0000

    Added index, changed styling

--- js/linearmotion.js
+++ js/linearmotion.js
@@ -1,73 +1,163 @@
 var size = 600;
 var circle;
 var paper;
-var proj;
 var clearID;
-var offGround;
 var fps = 60;
 var scale;
-var currentObject = 0;
+var firstObject;
+var secondObject;
 
 $(document).ready(function(){
-	
-	// Creates canvas 320 × 200 at 10, 50
+
 	paper = Raphael(160, 0, size, size);
-	
+
 	var backgroundRectangle = paper.rect(0, 0, size, size);
 	backgroundRectangle.attr("fill", "#bdbdbd");
 	backgroundRectangle.attr("stroke", "#000");
-	
+
 	circle = paper.circle(0, size, 10);
 	circle.attr("fill", "#f00");
 	circle.attr("stroke", "#fff");
-	
+
 });
 
+/*
+when the object selected in the check box changes this function runs to
+change the displayed fields
+*/
 function swapObjects(){
-	
+
 	var first = document.getElementById("first");
 	var second = document.getElementById("second");
-	
+
 	if(first.style.display != "none"){
-		
+
 		first.style.display = "none";
 		second.style.display = "block";
-		
+
 	}else{
-		
+
 		first.style.display = "block";
 		second.style.display = "none";
-		
+
 	}
 }
 
-function movingObject(){
-	this.u = 0;
-	this.s = 0;
-	this.a = 0;
+function movingObject(u, a, s){
+	this.u = u;
+	this.a = a;
+	this.s = s;
 }
 
-function run(){
-	
-}
 /*
-function addNew(){
-	
-	var value = document.getElementById("name").value;
-	
-	if(value == ""){
-		alert("Name cannot be empty");
+when the "Run simulation" button is pressed
+*/
+function run(){
+
+	if(!verifyInput()){
 		return;
 	}
-	
-	//var dropdown = document.getElementById("dropdown");
-	//var option = document.createElement("option");
-	//option.text = value;
-	//dropdown.add(option);
-	
-	var x = document.getElementById("dropdown");
-    var option = document.createElement("option");
-    option.text = value;
-    x.add(option);
+
+	parseInput();
+
+	clearID = setInterval(simulateStep(), (1/fps) * 1000);
+
 }
+
+/*
+gets values for the next step in the simulation
 */
+function simulateStep(){
+
+}
+
+
+function parseInput(){
+
+	firstObject = movingObject(
+		parseInt(document.getElementById("u")),
+		parseInt(document.getElementById("a")),
+		parseInt(document.getElementById("s"))
+	);
+
+	if(document.getElementById("enableSecond").checked == true){
+		secondObject = movingObject(
+			parseInt(document.getElementById("u1")),
+			parseInt(document.getElementById("a1")),
+			parseInt(document.getElementById("s1"))
+		);
+	}
+
+}
+
+/*
+check that all input is correctly formed
+if errors are found format an error alert saying where the errors are
+*/
+function verifyInput(){
+
+	var alertMessage = "";
+
+	var value = parseInt(document.getElementById("u").value);
+
+	if(isNaN(value) || value == ""){
+		alertMessage +=
+		"First object:\n\tInitial speed must be a number\n";
+	}
+
+	value = parseInt(document.getElementById("a").value);
+
+	if(isNaN(value) || value == ""){
+		alertMessage += "\tAcceleration must be a number\n";
+	}
+
+	value = parseInt(document.getElementById("s").value);
+
+	if(isNaN(value) || value == ""){
+		alertMessage += "\tInitial position angle must be a number\n";
+	}
+
+	/* if the enable second check box is checked  */
+	if(document.getElementById("enableSecond").checked == true){
+
+		var value = parseInt(document.getElementById("u1").value);
+
+		if(isNaN(value) || value == ""){
+			alertMessage +=
+			"\nSecond object:\n\tInitial speed must be a number\n";
+		}
+
+		value = parseInt(document.getElementById("a1").value);
+
+		if(isNaN(value) || value == ""){
+			alertMessage += "\tAcceleration must be a number\n";
+		}
+
+		value = parseInt(document.getElementById("s1").value)
+
+		if(isNaN(value) || value == ""){
+			alertMessage +=
+			"\tInitial position angle must be a number\n";
+		}
+
+	}
+
+	if(alertMessage != ""){
+		alert(alertMessage);
+		return false;
+	}
+
+}
+
+function clearInput(){
+
+	document.getElementById("u").value = "";
+	document.getElementById("a").value = "";
+	document.getElementById("s").value = "";
+
+	document.getElementById("enableSecond").checked = false;
+	document.getElementById("u1").value = "";
+	document.getElementById("a1").value = "";
+	document.getElementById("s1").value = "";
+
+}
--- js/projectiles.js
+++ js/projectiles.js
@@ -9,7 +9,6 @@ var scale;
 
 $(document).ready(function(){
 	
-	// Creates canvas 320 × 200 at 10, 50
 	paper = Raphael(160, 0, size, size);
 	
 	var backgroundRectangle = paper.rect(0, 0, size, size);
@@ -23,8 +22,8 @@ $(document).ready(function(){
 });
 
 function Projectile(u, projectileAngle){
-		this.ux = u * Math.cos(projectileAngle * (Math.PI / 180)); // speed in x plane
-		this.uy = u * Math.sin(projectileAngle * (Math.PI / 180)); // speed in y plane
+		this.ux = u * Math.cos(projectileAngle * (Math.PI / 180));
+		this.uy = u * Math.sin(projectileAngle * (Math.PI / 180));
 		this.vx = 0;
 		this.vy = 0;
 		this.pa = projectileAngle;
@@ -33,19 +32,26 @@ function Projectile(u, projectileAngle){
 		this.sy = 0; // distance in y plane
 		this.time = 0;
 		this.range = this.ux * this.tof;
-		this.height = (this.uy * (this.tof / 2) + (0.5 * -9.8 * (this.tof / 2) * (this.tof / 2)));
+		this.height = 
+			(this.uy * (this.tof / 2) 
+			+ (0.5 * -9.8 * (this.tof / 2) 
+			* (this.tof / 2)));
 		this.startHeight = -1;
 		this.slopeAngle = -1;
 }
 
-function getNextPosition(){
+/*
+gets values for the next step in the simulation
+*/
+function simulateStep(){
 	
 	if(proj.time < proj.tof){
 		
 		proj.vx = proj.ux;
 		proj.vy = proj.uy + (-9.8 * proj.time);
 		proj.sx = (proj.ux * proj.time);
-		proj.sy = (proj.uy * proj.time) + (0.5 * -9.8 * proj.time * proj.time);
+		proj.sy = (proj.uy * proj.time) 
+			+ (0.5 * -9.8 * proj.time * proj.time);
 		proj.time = proj.time + (1/fps);
 	
 		circle.attr("cx", proj.sx * scale);
@@ -71,14 +77,19 @@ function getNextPosition(){
 	
 }
 
-function getNextPositionOffGround(){
+/*
+gets values for the next step in the simulation
+specific for when the projectile starts off the ground
+*/
+function simulateStepOffGround(){
 	
 	if(proj.time < proj.tof){
 		
 		proj.vx = proj.ux;
 		proj.vy = proj.uy + (-9.8 * proj.time);
 		proj.sx = (proj.ux * proj.time);
-		proj.sy = (proj.uy * proj.time) + (0.5 * -9.8 * proj.time * proj.time);
+		proj.sy = (proj.uy * proj.time) 
+			+ (0.5 * -9.8 * proj.time * proj.time);
 		proj.time = proj.time + (1/fps);
 	
 		circle.attr("cx", proj.sx * scale);
@@ -104,16 +115,23 @@ function getNextPositionOffGround(){
 	
 }
 
-function runSimulation(){
+/*
+called when the "Run simulation" button is pressed
+*/
+function run(){
 	
 	if(verifyInput() == false){
 		return;
 	}
 	
-	proj = new Projectile(parseInt(document.getElementById("u").value), parseInt(document.getElementById("angle").value));
+	proj = new Projectile(parseInt(
+		document.getElementById("u").value),
+		parseInt(document.getElementById("angle").value)
+	);
 	
 	if(document.getElementById("offGround").checked){
-		proj.startHeight = parseInt(document.getElementById("startHeight").value);
+		proj.startHeight = 
+			parseInt(document.getElementById("startHeight").value);
 		getOffGroundValues();
 	}
 	
@@ -125,26 +143,35 @@ function runSimulation(){
 	document.getElementById("vy").innerHTML = "vy: ";
 	document.getElementById("sx").innerHTML = "sx: ";
 	document.getElementById("sy").innerHTML = "sy: ";
-	document.getElementById("tof").innerHTML = "TOF: " + proj.tof.toFixed(3);
-	document.getElementById("range").innerHTML = "Range: " + proj.range.toFixed(3);
-	document.getElementById("height").innerHTML = "Max height: " + proj.height.toFixed(3);
-	document.getElementById("scale").innerHTML = "Scale: 1 metre:" + scale.toFixed(3) + " pixels";
+	document.getElementById("tof").innerHTML = 
+		"TOF: " + proj.tof.toFixed(3);
+	document.getElementById("range").innerHTML = 
+		"Range: " + proj.range.toFixed(3);
+	document.getElementById("height").innerHTML = 
+		"Max height: " + proj.height.toFixed(3);
+	document.getElementById("scale").innerHTML = 
+		"Scale: 1 metre:" + scale.toFixed(3) + " pixels";
 	
 	if(proj.startHeight != -1){
-
-		clearID = setInterval(getNextPositionOffGround, (1/fps) * 1000);
+		clearID = setInterval(simulateStepOffGround, (1/fps) * 1000);
 	}else{
-		clearID = setInterval(getNextPosition, (1/fps) * 1000);
+		clearID = setInterval(simulateStep, (1/fps) * 1000);
 	}
 	
 }
 
+/*
+gets constant values for when the specific case where the projectile starts
+off the ground
+*/
 function getOffGroundValues(){
 	
-	proj.tof = (-proj.uy - Math.sqrt((proj.uy * proj.uy) - (4*0.5*-9.8*proj.startHeight))) / -9.8;
+	proj.tof = (-proj.uy - Math.sqrt((proj.uy * proj.uy) 
+		- (4*0.5*-9.8*proj.startHeight))) / -9.8;
 	proj.range = proj.ux * proj.tof;
 	var tempTOF = (2 * proj.uy) / 9.8
-	proj.height = (proj.uy * (tempTOF / 2) + (0.5 * -9.8 * (tempTOF / 2) * (tempTOF / 2)));
+	proj.height = (proj.uy * (tempTOF / 2) 
+		+ (0.5 * -9.8 * (tempTOF / 2) * (tempTOF / 2)));
 	
 }
 	
@@ -166,39 +193,43 @@ function verifyInput(){
 
 	var alertMessage = "";
 	
-	var value = document.getElementById("u").value;
+	var value = parseInt(document.getElementById("u").value);
 	
 	if(isNaN(value) || value == "" || value <= 0){
 		alertMessage += "Initial speed must be a number and be > 0\n";
 	}
 	
-	value = document.getElementById("angle").value;
+	value = parseInt(document.getElementById("angle").value);
 	
 	if(isNaN(value) || value == "" || value <= 0 || value > 90){
-		alertMessage += "Projectile angle must be a number and be > 0 and <= 90\n";
+		alertMessage += 
+		"Projectile angle must be a number and be > 0 and <= 90\n";
 	}
 	
-	/* if the sloped check box is checked then verify the value of slope angle */
+	/* if the sloped check box is checked */
 	if(document.getElementById("isSloped").checked == true){
 		
 		var angleTotal = value;
-		value = document.getElementById("slopeAngle").value;
-		angleTotal += value; /* add the two angles together and make sure they are < 90 */
+		value = parseInt(document.getElementById("slopeAngle").value);
+		/* add the two angles together and make sure they are < 90 */
+		angleTotal += value;
 		
 		if(isNaN(value) || value == "" || value <= 0 || value > 90){
-			alertMessage += "Slope angle must be a number and be > 0 and <= 90\n";
+			alertMessage += 
+			"Slope angle must be a number and be > 0 and <= 90\n";
 		}
 		
 		if(angleTotal >= 90){
-			alertMessage += "Slope angle + projectile angle must be < 90";
+			alertMessage += 
+			"Slope angle + projectile angle must be < 90";
 		}
 		
 	}
 	
-	/* if the off ground check box is checked then verify the value of starting height */
+	/* if the off ground check box is checked */
 	if(document.getElementById("offGround").checked == true){
 		
-		value = document.getElementById("startHeight").value;
+		value = parseInt(document.getElementById("startHeight").value);
 		if(isNaN(value) || value == "" || value <= 0){
 			alertMessage += "Starting height must be a number and be > 0\n";
 		}
@@ -213,10 +244,12 @@ function verifyInput(){
 }
 
 function clearInput(){
+	
 	document.getElementById("u").value = "";
 	document.getElementById("angle").value = "";
 	document.getElementById("isSloped").checked = false;
 	document.getElementById("slopeAngle").value = "";
 	document.getElementById("offGround").checked = false;
 	document.getElementById("startHeight").value = "";
+	
 }
