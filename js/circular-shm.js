9a85413f6e010f1275869367e1bf756fae40309d
Mikey Rooney <m.rooney93@gmail.com>
2015-04-20 22:21:51 +0100

    Fix resizing issue on mobile devices

--- js/circular-shm.js
+++ js/circular-shm.js
@@ -54,11 +54,11 @@ function getNewSize(){
 	var x;
 	oldSize = size;
 	oldScale;
-		
-	size = w - 250 - 10 - 165;
 	
-	if(size + 80 > h){
-		size = h - 80;
+	if(w > h){
+		size = w - 250 - 10 - 165;
+	}else{
+		size = h - 80 - 250 - 10 - 165;
 	}	
 	
 	if(state){
